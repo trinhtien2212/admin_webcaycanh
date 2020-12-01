d1e86ebe6d2da150e24230a136d1d4267da516b8
trinhtien2212 <trinhtien6236@gmail.com>
2020-12-01 08:24:05 +0700

    Tien sua doi, lien ket file

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -120,26 +120,26 @@
 
 	// Sidebar Slimscroll
 
-	if($slimScrolls.length > 0) {
-		$slimScrolls.slimScroll({
-			height: 'auto',
-			width: '100%',
-			position: 'right',
-			size: '7px',
-			color: '#ccc',
-			allowPageScroll: false,
-			wheelStep: 10,
-			touchScrollStep: 100
-		});
-		var wHeight = $(window).height() - 60;
-		$slimScrolls.height(wHeight);
-		$('.sidebar .slimScrollDiv').height(wHeight);
-		$(window).resize(function() {
-			var rHeight = $(window).height() - 60;
-			$slimScrolls.height(rHeight);
-			$('.sidebar .slimScrollDiv').height(rHeight);
-		});
-	}
+	// if($slimScrolls.length > 0) {
+	// 	$slimScrolls.slimScroll({
+	// 		height: 'auto',
+	// 		width: '100%',
+	// 		position: 'right',
+	// 		size: '7px',
+	// 		color: '#ccc',
+	// 		allowPageScroll: false,
+	// 		wheelStep: 10,
+	// 		touchScrollStep: 100
+	// 	});
+	// 	var wHeight = $(window).height() - 60;
+	// 	$slimScrolls.height(wHeight);
+	// 	$('.sidebar .slimScrollDiv').height(wHeight);
+	// 	$(window).resize(function() {
+	// 		var rHeight = $(window).height() - 60;
+	// 		$slimScrolls.height(rHeight);
+	// 		$('.sidebar .slimScrollDiv').height(rHeight);
+	// 	});
+	// }
 	
 	// Small Sidebar
 
